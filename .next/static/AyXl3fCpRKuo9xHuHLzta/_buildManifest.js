9644a9616804aad858864544c4895292ec4601d2
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-03-16 22:26:17 -0400

    nav bld

--- .next/static/AyXl3fCpRKuo9xHuHLzta/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/794-fe4d6e8925c813d1.js","static/css/357e6fefc9584afe.css","static/chunks/pages/index-dda4dbb5d8a2e026.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
