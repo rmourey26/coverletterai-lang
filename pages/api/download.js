893ab156475cf547d9e6f6d49e65a93609a18fdb
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-03-17 18:53:26 +0000

    ...

--- pages/api/download.js
+++ pages/api/download.js
@@ -3,27 +3,3 @@ export default function createFileFromText (text, fileName, mimeType='text/plain
   const fileUrl = URL.createObjectURL(data);
   return { name: fileName, data, fileUrl };
 }
-
-async function downloadChatGPTMessages(apiKey, messages, fileName, mimeType) {
-  try {
-    const response = await fetch ("/api/chat", apiKey, messages);
-    const text = response.choices[0].text;
-    const file = createFileFromText(text, fileName, mimeType);
-
-    // Download file
-    const link = document.createElement('a');
-    link.href = file.fileUrl;
-    link.download = file.name;
-    link.click();
-
-    // Cleanup
-    URL.revokeObjectURL(file.fileUrl);
-  } catch (error) {
-    console.error('An error occurred:', error.message);
-  }
-}
-const apiKey = 'your_openai_api_key';
-const fileName = 'chatgpt_messages.txt';
-const mimeType = 'text/plain';
-
-downloadChatGPTMessages(apiKey, messages, fileName, mimeType);
